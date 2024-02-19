e8aecd5f5ae1da9d1f817c104d225a81b760d787
Ren-Logronio <reinhart.logronio@msugensan.edu.ph>
2024-02-19 22:11:06 +0800

    xx

--- src/app.tsx
+++ src/app.tsx
@@ -6,7 +6,7 @@ export function App() {
     <>
       <div className="absolute top-0 bottom-0 h-screen w-full overflow-hidden">
         <div className="absolute bg-transparent top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-[100] flex flex-col space-y-4">
-          <p className="text-center text-[33px] font-black paint-fsm font-krona text-stroke text-stroke-teal text-fill-transparent text-lg">JITS WEBSITE</p>
+          <p className="text-center !text-[33px] font-black paint-fsm font-krona text-stroke text-stroke-teal text-fill-transparent text-lg">JITS WEBSITE</p>
           <p className="text-center font-kumbh text-[#35FFEB]">{downMessage}</p>
         </div>
         <div class="absolute h-screen w-full z-[25] opacity-80 bg-gray-900"></div>
